9a8e138e6486b5311055f93deed50ff051854724
Berkay KIZGIR <berkaykizgir@outlook.com>
2022-05-28 14:40:44 +0300

    questions

--- lib/quiz_screen/quiz_contents.dart
+++ lib/quiz_screen/quiz_contents.dart
@@ -404,194 +404,322 @@ class QuizContents {
       'correct': 2
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'In Iterator, hasMoreElements() method of Enumeration has been changed to:',
+      'answer': ['hasNextElement()', 'isNext()', 'hasNext()', 'name remains same'],
+      'correct': 2
+    },
+    {
+      'question': 'TreeSet internally uses that one to store elements?',
+      'answer': ['HashMap', 'LinkedHashMap', 'TreeMap', 'Map'],
+      'correct': 2
+    },
+    {
+      'question': 'HashSet internally uses?',
+      'answer': ['HashMap', 'LinkedHashMap', 'TreeMap', 'Map'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'An attempt to add the null key to a TreeSet will result in:',
+      'answer': ['Will compile', 'Compile time Exception', 'Error', 'Runtime - NullPointerException'],
+      'correct': 3
+    },
+    {
+      'question': 'TreeSet maintains which order?',
+      'answer': ['Ascending Order', 'Descending Order', 'None', 'Both'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'Enumeration returned by ArrayList is',
+      'answer': ['Fail-fast', 'Fail-safe', 'None', 'Both'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'LinkedHashMap permits',
+      'answer': ['One null key', 'Many null values', 'Both', 'None'],
+      'correct': 2
+    },
+    {
+      'question': 'Hashtable permits',
+      'answer': ['One null key', 'Many null values', 'None', 'Both'],
+      'correct': 2
+    },
+    {
+      'question': 'How are you able to sort given HashMap on basis of values',
+      'answer': [
+        'Implement Comparator interface and override its compare method',
+        'It is not possible',
+        'Implement Comparator interface and override its compareTo method',
+        'Implement Comparator and Comparable interface'
+      ],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'The Comparator interface contains the method?',
+      'answer': ['compareWith()', 'compareTo()', 'compare()', 'toWith()'],
+      'correct': 2
+    },
+    {
+      'question': 'What will Collections.sort internally uses when number of elements are less than 7?',
+      'answer': ['Insertion sort', 'Merge sort', 'Quick sort', 'Bubble sort'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'An attempt to add null key to a TreeMap will result in:',
+      'answer': ['Compile time Exception', 'Error', 'Runtime - NullPointerException', 'None'],
+      'correct': 2
+    },
+    {
+      'question':
+          'What will be the output of the following code -\nimport java.util.Map;\nimport java.util.TreeMap;\npublic class TreeMapTest {\npublic static void main (String args[])\n{\nMap<Integer,String> m= new TreeMap<Integer,String>();\nm.put(11, “audi”);\nm.put(null, null);\nm.put(11,”bmw”);\nm.put(null,”fer”);\nSystem.out.println(m.size());\nSystem.out.println(m);\n}\n}\n',
+      'answer': ['Compile time exception', '1', '2', 'Runtime Exception'],
+      'correct': 3
+    },
+    {
+      'question': 'Which of those is synchronized and does not allow null elements?',
+      'answer': ['Vector', 'CopyOnWriteArraySet', 'CopyOnWriteArrayList', 'ConcurrentSkipListSet'],
+      'correct': 3
+    },
+    {
+      'question': 'Which of these is synchronised and isn’t sorted by natural order?',
+      'answer': ['EnumSet', 'LinkedList', 'ConcurrentSkipListSet', 'Vector'],
+      'correct': 3
+    },
+    {
+      'question':
+          'What will be the output of the following code -\nimport java.util.Iterator;\nimport java.util.Map;\nimport java.util.concurrent.ConcurrentSkipListMap;\npublic class ConcurrentSkipMapTest {\npublic static void main (String args[])\n{\nMap<Integer,String>concurrentSkipListMap= new ConcurrentSkipListMap<Integer,String>();\nconcurrentSkipListMap.put(11,”audi”);\nIterator<Integer> keyIterator = concurrentSkipListMap.keySet().iterator();\nwhile (keyIterator.hasNext())\n{\nSystem.out.print(keyIterator.next());\nconcurrentSkipListMap.put(13,”bmw”);\n}\n}\n}\n',
+      'answer': ['11 followed by ConcurrentModificationException', '11', 'ConcurrentModificationException', 'Compile time exception'],
+      'correct': 1
+    },
+    {
+      'question':
+          'What will be the output of the following code -\nimport java.util.EnumMap;\nimport java.util.Map;\npublic class EnumMapExample {\npublic enum Days\n{\nMonday;\n}public static void main (String args[])\n{\nMap<Days,String> daysEnumMap = new daysEnumMap<Days,String>(Days.class);\ndaysEnumMap.put(Days.Monday,”Day1″);\ndaysEnumMap.put(Days.Tuesday,”Day2″);\nSystem.out.println(“daysEnumMap.get(Days.Monday):”+ daysEnumMap.get(Days.Monday));\nSystem.out.println(“daysEnumMap.containsKey(Days.Monday):”+\ndaysEnumMap.containsKey(Days.Tuesday)) ;\n}\n}\n',
+      'answer': [
+        'daysEnumMap.get(Days.Monday) : Day1 daysEnumMap.containsKey(Days.Monday) : true',
+        'daysEnumMap.get(Days.Monday) : Day1 daysEnumMap.containsKey(Days.Monday) : false',
+        'Runtime Exception',
+        'Compile time exception'
+      ],
+      'correct': 3
+    },
+    {
+      'question':
+          'What will be the output of the following code -\nimport java.util.List;\nimport java.util.concurrent.CopyOnWriteArrayList;\npublic class CopyOnWriteArrayListTest\n{\npublic static void main (String args[])\n{\nList<String> copyOnWriteArrayList = new CopyOnWriteArrayList<String>();\ncopyOnWriteArrayList.add(” Ind “);\ncopyOnWriteArrayList.add(” usa “);\ncopyOnWriteArrayList.add(null);\nfor(String string : copyOnWriteArrayList)\n{\nSystem.out.print(string+””);\ncopyOnWriteArrayList.add(“newEle3”) ;\n}\n}\n}\n',
+      'answer': ['ind followed by NullPointerException', 'ind followed by ConcurrentModificationException', 'ind usa null', 'Compile time exception'],
+      'correct': 2
+    },
+    {
+      'question': 'In ListIterator, when a call to next() or previous(), if a call is made to set(E element) followed by add() or remove() what happens?',
+      'answer': ['Compilation error', 'UnsupportedOperationException', 'ConcurrentModificationException', 'Syntax Error'],
+      'correct': 1
+    },
+    {
+      'question': 'Which interface should be implemented for sorting on basis of many criteria’s?',
+      'answer': ['Comparator', 'Comparable', 'Serializable', 'None'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'Can two threads on same ConcurrentHashMap object access it concurrently',
+      'answer': [
+        'We need to add additional functionality to achieve the same.',
+        'Its not possible',
+        'Different threads will access different|completely different} segments concurrently',
+        'All'
+      ],
+      'correct': 2
+    },
+    {
+      'question': 'LinkedHashSet implements:',
+      'answer': ['Set', 'Cloneable', 'Serializable', 'All'],
+      'correct': 3
+    },
+    {
+      'question':
+          'What will be the output of the following code -\nimport java.util.Iterator;\nimport java.util.Set;\nimport java.util.concurrent.CopyOnWriteArraySet;\npublic class CopyOnWriteArraySetTest\n{\npublic static void main (String args[])\n{\nSet<String> copyOnWriteArraySet = new CopyOnWriteArraySet<String>();\ncopyOnWriteArraySet.add(” a “);\ncopyOnWriteArraySet.add ( ” b” );\nIterator<String> iterator = copyOnWriteArraySet.iterator();\nwhile(iterator.hasNext())\n{\ncopyOnWriteArraySet.add( ” c ” );\nSystem.out.println(iterator.next());\n}\n}\n}\n',
+      'answer': ['a followed by exception', 'a', 'a b', 'Compile time exception'],
+      'correct': 2
+    },
+    {
+      'question': 'In WeakHashMap',
+      'answer': [
+        'An entry are automatically removed by garbage collector',
+        'Behaves normally like HashMap',
+        'An entry are never removed by garbage collector',
+        'All'
+      ],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'A EnumSet',
+      'answer': [
+        'Can use enum and non enum type keys',
+        'Cannot use enum type keys',
+        'Is specialised Set implementation for use with enum type keys only',
+        'All'
+      ],
+      'correct': 2
+    },
+    {
+      'question': 'Iterator returned by HashSet is',
+      'answer': ['Fail-fast', 'Fail-safe', 'None', 'Both'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'Iterator returned by CopyOnWriteArraySet is',
+      'answer': ['Fail-fast', 'Fail-safe', 'None', 'Both'],
+      'correct': 1
+    },
+    {
+      'question': 'Iterator returned by TreeSet is',
+      'answer': ['Fail-fast', 'Fail-safe', 'None', 'Both'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'Iterator returned by LinkedHashSet is',
+      'answer': ['Fail-fast', 'Fail-safe', 'None', 'Both'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'Iterator returned by ConcurrentSkipListSet is',
+      'answer': ['Fail-fast', 'Fail-safe', 'None', 'Both'],
+      'correct': 1
+    },
+    {
+      'question': 'Which List is synchronized?',
+      'answer': ['Vector', 'CopyOnWriteArrayList', 'Both', 'None'],
+      'correct': 2
+    },
+    {
+      'question': 'Which Set is synchronized?',
+      'answer': ['CopyOnWriteArraySet', 'ConcurrentSkipListSet', 'Both', 'None'],
+      'correct': 2
+    },
+    {
+      'question':
+          'What will be the output of the following code -\nimport java.util.List;\nimport java.util.ListIterator;\nimport java.util.ArrayList;\npublic class ArrayListTest\n{\npublic static void main (String args[])\n{\nList<String> arrayList = new ArrayList<String>();\narrayList.add(“a”);\narrayList.add(“b”);\nListIterator<String> listIterator = arrayList.listIterator();\nwhile (listIterator.hasNext())\n{\nSystem.out.println(listIterator.next());\nlistIterator.previous( );\n}\n}\n}\n',
+      'answer': ['a b a', 'a b', 'Will print ‘a’ infinite times', 'a'],
+      'correct': 2
+    },
+    {
+      'question': 'What is the significance of using instanceOf operator and getClass() in equals method?',
+      'answer': [
+        'InstanceOf can return true for comparing current class with its subclass as well',
+        'getClass() can return true only if exactly same class is compared',
+        'getClass() can return false when compared with subclass',
+        'All'
+      ],
+      'correct': 3
+    },
+    {
+      'question':
+          'What will be the output of the following code -\nimport java.util.Iterator;\nimport java.util.Map;\nimport java.util.concurrent.ConcurrentSkipListMap;\npublic class ConcurrentSkipMapTest {\npublic static void main (String args[])\n{\nMap<Integer,String> concurrentSkipListMap= new ConcurrentSkipListMap<Integer,String>();\nconcurrentSkipListMap.put(11,”audi”);\nconcurrentSkipListMap.put(44,”null”);\nIterator<Integer> keyIterator = concurrentSkipListMap.keySet().iterator();\nwhile (keyIterator.hasNext())\n{\nSystem.out.print(keyIterator.next());\n}\n}\n}\n',
+      'answer': ['0 null', '1 null', '2 null', '11 44'],
+      'correct': 2
+    },
+    {
+      'question':
+          'What will be the output of the following code -\nimport java.util.HashMap;\npublic class Employee {\nprivate String name;\nprivate String name;\n{\nthis.name = name;\n}\n}\nclass EmployeeClass\npublic static void main(String args[])\n{\nHashMap<Employee,String> hm=new HashMap<Employee,String>();\nhm.put(new Employee(“a”),”emp1″);\nhm.put(new Employee(“b”),”emp2″);\nhm.put(new Employee(“a”),”emp1 overridden”);\nSystem.out.println(hm.size());\nSystem.out.println(hm.get(new Employee(“a”)));\n}\n}\n',
+      'answer': ['Compile time exception', 'a b c d e 1', 'a b c d e 2', '3 null'],
+      'correct': 3
+    },
+    {
+      'question': 'Which of those isn’t an interface within the Collections Framework?',
+      'answer': ['Collection', 'Group', 'Set', 'List'],
+      'correct': 1
+    },
+    {
+      'question': 'Which interface restricts duplicate elements?',
+      'answer': ['Set', 'List', 'Map', 'All'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'Which of those assortment categories has the flexibility to grow dynamically?',
+      'answer': ['Array', 'Arrays', 'ArrayList', 'None'],
+      'correct': 2
+    },
+    {
+      'question': 'The efficiency and accuracy of a HashMap are often warranted with:',
+      'answer': ['Override equals method', 'Override hashCode method', 'None', 'Both'],
+      'correct': 3
+    },
+    {
+      'question': 'A HashMap permits the existence of:',
+      'answer': ['null values', 'one null key', 'None of these', 'Both of these'],
+      'correct': 3
+    },
+    {
+      'question': 'In an Iterator what implementation will traverse a collection in each direction?',
+      'answer': ['Iterator', 'ListIterator', 'SetIterator', 'MapIterator'],
+      'correct': 1
+    },
+    {
+      'question': 'The Comparable interface contains which method:',
+      'answer': ['toCompare', 'compare', 'compareTo()', 'compareWith'],
+      'correct': 2
+    },
+    {
+      'question': 'Which is quicker and uses less memory?',
+      'answer': ['ListEnumeration', 'Iterator', 'Enumeration', 'ListIterator'],
+      'correct': 2
+    },
+    {
+      'question': 'What Iterator will throw a ConcurrentModificationException?',
+      'answer': ['Fail-fast Iterators', 'Fail-safe Iterators', 'Both of these', 'None of these'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
+      'question': 'State the number of default segments in Concurrent HashMap.',
+      'answer': ['12', '32', '4', '16'],
+      'correct': 1
+    },
+    {
+      'question': 'Which is best suited to a multi-threaded environment?',
+      'answer': ['WeakHashMap', 'Hashtable', 'HashMap', 'ConcurrentHashMap'],
+      'correct': 3
+    },
+    {
+      'question': 'The default capacity of a Vector is:',
+      'answer': ['10', '12', '8', '16'],
       'correct': 0
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
+      'question': 'Which doesn’t implement the collection interface?',
+      'answer': ['List', 'Map', 'Set', 'None of these'],
+      'correct': 1
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
+      'question': 'The default capacity of an ArrayList is:',
+      'answer': ['12', '16', '1', '10'],
+      'correct': 3
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
+      'question': 'Which of the following provides higher performance for the insertion and removal from the center of the list?',
+      'answer': ['Vector', 'ArrayList', 'LinkedList', 'None'],
+      'correct': 2
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
+      'question': 'An unordered array has a time complexity of:',
+      'answer': ['O(log n)', 'O(n)', 'O(n + 1)', 'O(1)'],
+      'correct': 1
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
+      'question': 'The add and take away methods of TreeSet have a time complexity of:',
+      'answer': ['O(n)', 'O(n + 1)', 'O(1)', 'O(log n)'],
+      'correct': 3
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
+      'question': 'After resizing, size of ArrayList is increased by :',
+      'answer': ['200%', '50%', '100%', 'None of these'],
+      'correct': 1
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
+      'question': 'After resizing, size of Vector is accumulated by:',
+      'answer': ['200%', '100%', '50%', 'None of these'],
+      'correct': 1
     },
     {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
-    },
-    {
-      'question': '',
-      'answer': ['', '', '', ''],
-      'correct': 0
+      'question': 'Deque and Queue are derived from:',
+      'answer': ['AbstractList', 'Collection', 'AbstractCollection', 'List'],
+      'correct': 1
     },
   ];
 }
